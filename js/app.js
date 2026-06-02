de092fc08793db9dd3603b7cacf3497f1287f1b9
erikfritsche-0394596 <erik.fritsche@atlantis-berlin.de>
2026-06-02 23:34:23 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -88,7 +88,7 @@ function formatDate(date) {
 function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
+      navigator.serviceWorker.register('/herbi/sw.js')
         .then(reg => console.log('SW registered:', reg.scope))
         .catch(err => console.log('SW registration failed:', err));
     });
